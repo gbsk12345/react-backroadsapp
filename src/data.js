b4c3def965b659fc16b3d6690661ca0d48505e2c
gbsk12345 <104025468+gbsk12345@users.noreply.github.com>
2023-06-20 06:35:25 +0530

    adding files

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.facebook.com/", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  {
+    id: 3,
+    href: "https://www.squarespace.com/?clickid=VMOQ-x074xyPUe11dh0VMRcPUkFxD8xkZ2KEUc0&irgwc=1&utm_medium=pp&utm_source=SKS%20DIGITAL%20PTE%20LTD.&utm_campaign=SKS%20DIGITAL%20PTE%20LTD.&channel=pp&subchannel=2363236&source=SKS%20DIGITAL%20PTE%20LTD.",
+    icon: "fab fa-squarespace",
+  },
+];
