e2dd0a4a790c26f392499063c357ddde00f9fe2d
gbsk12345 <104025468+gbsk12345@users.noreply.github.com>
2023-06-20 16:51:26 +0530

    adding entire completed project

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -12,5 +17,72 @@ export const socialLinks = [
     id: 3,
     href: "https://www.squarespace.com/?clickid=VMOQ-x074xyPUe11dh0VMRcPUkFxD8xkZ2KEUc0&irgwc=1&utm_medium=pp&utm_source=SKS%20DIGITAL%20PTE%20LTD.&utm_campaign=SKS%20DIGITAL%20PTE%20LTD.&channel=pp&subchannel=2363236&source=SKS%20DIGITAL%20PTE%20LTD.",
     icon: "fab fa-squarespace",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: "6",
+    price: "2100",
+  },
+
+  {
+    id: 2,
+    image: tour2,
+    date: "october 1th, 2020",
+    title: "Best of java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: "11",
+    price: "1400",
+  },
+
+  {
+    id: 3,
+    image: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: "8",
+    price: "5000",
+  },
+
+  {
+    id: 4,
+    image: tour4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "kenya",
+    duration: "20",
+    price: "3300",
   },
 ];
